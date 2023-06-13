4eaeca4fc8a6a6c521e229d41abcf2f56f321c42
Sharjeel-22 <pcraboot55441@gmail.com>
2023-06-13 20:26:44 +0500

    add server in deployments

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const path = require('path');
 const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
 const app = express();
 app.use(express.static(__dirname + '/dist/frontend'));
 app.get('/*',(req,res) => {
     res.sendFile(path.join(__dirname, 'dist/frontend','index.html'));
 })
-app.listen(5000);
+app.listen(process.env.PORT || 5000);
