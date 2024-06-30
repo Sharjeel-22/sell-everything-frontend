7e7187619b1b7528e3c6b0c86bec97bbc168ce1a
Sharjeel-22 <pcraboot55441@gmail.com>
2024-06-30 11:30:11 -0700

    Created user dashboard and related components

---
+++ src/app/user-sales-dashboard/dashboard/user-revenue/user-revenue.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-revenue',
+  templateUrl: './user-revenue.component.html',
+  styleUrls: ['./user-revenue.component.css']
+})
+export class UserRevenueComponent {
+
+}
