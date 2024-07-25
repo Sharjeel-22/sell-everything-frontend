13c107e8c563d06a59b103a8218bc863ba6bf1db
Sharjeel-22 <pcraboot55441@gmail.com>
2024-07-25 21:37:22 +0500

    added test case

--- src/app/resourceService/resource.service.spec.ts
+++ src/app/resourceService/resource.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
-
-import { ResourceService } from './resource.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing'; // Correct module for testing HTTP
+import { ResourceService } from './resource.service'; // Ensure this is a service
 
 describe('ResourceService', () => {
   let service: ResourceService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule], // Import the module for HTTP testing
+      providers: [ResourceService], // Provide the service here
+    });
     service = TestBed.inject(ResourceService);
   });
 
